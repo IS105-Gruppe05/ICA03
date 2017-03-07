5ae71a3f4ad407dbe437df2f57d41765913736e3
Brede Meli <bredemeli@bredes-mbp.uia.no>
2017-03-07 17:23:43 +0100

    jada

--- ascii/ascii/ascii.go
+++ ascii/ascii/ascii.go
@@ -1,29 +1,27 @@
 package ascii
 
-import (
-	"fmt"
-)
+import "fmt"
 
-//comment
 const Ascii = "\x00\x01\x02\x03\x04\x05\x06\x07\x08\x09\x0a\x0b\x0c\x0d\x0e\x0f" +
 	"\x10\x11\x12\x13\x14\x15\x16\x17\x18\x19\x1a\x1b\x1c\x1d\x1e\x1f" +
 	` !"#$%&'()*+,-./0123456789:;<=>?` +
 	`@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_` +
 	"`abcdefghijklmnopqrstuvwxyz{|}~\x7f"
 
+const a = "\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
+
 // Funksjon tar en streng med kun ASCII tegn og lager en utskrift på
 // følgende format:
 // [ascii-kode heksadesimalt med store bokstaver A-F][mellomrom]
 // [symbol for ascii-kode][mellomrom][ascii-kode binært][linjeskift]
 // Eksempel (bruk denne funksjonen i en main.go fil):
-//  …
+//	…
 // 3E > 111110
 // 3F ? 111111
 // 40 @ 1000000
 // ...
-
-//comment
 func IterateOverASCIIStringLiteral(sl string) {
+	// Kode for Oppgave 1a
 	for i := 0; i < len(sl); i++ {
 		fmt.Printf("%X %+q %b \n", sl[i], sl[i], sl[i])
 	}
@@ -38,14 +36,8 @@ func IterateOverASCIIStringLiteral(sl string) {
 // som inneholder kun ASCII tegn (ikke utvidet ASCII).
 // Gjelder oppgave 1b
 
-//Hei = 48 65 69    func DecodeString(s string) ([]byte, error)
-
-func GreetingASCII() string {
-	fmt.Println()
-	a := "\x22\x48\x65\x6C\x6C\x6F\x20\x3A\x2D\x29\x22"
+func GreetingASCII() {
 	for i := 0; i < len(a); i++ {
 		fmt.Printf("%c", a[i])
 	}
-	fmt.Println()
-	return a
 }
