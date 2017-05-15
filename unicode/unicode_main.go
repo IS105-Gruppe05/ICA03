c63eb5d4327af101c964bade74fc9bc8f0815f22
hyttapower <mortjm14@student.uia.no>
2017-05-15 15:36:01 +0200

    Update unicode_main.go

--- unicode/unicode_main.go
+++ unicode/unicode_main.go
@@ -7,7 +7,7 @@ import "os"
 //is := "\x93\x6e\x6f\xf5\x75\x72\x20\x6f\x20\x73\x75\xf5\x75\x73\x93"
 //jp := "\xE3\x81\xA8\xE5\x8D\x97\xe5\x8c\x97"
 //language := "“norður og suður”
-
+//Må huske å ha med argument når man kjører programmet. 
 func main() {
 
 	var t string = os.Args[1]
